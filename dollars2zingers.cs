87f8228c7364de7f7e68793d99177285d7d470c2
logArythm557 <jake.ewhite@gmail.com>
2017-08-01 21:44:29 +0930

    Update dollars2zingers.cs
    minor formatting fixes, still need to fix formatting from final Console.WriteLine()

--- dollars2zingers.cs
+++ dollars2zingers.cs
@@ -16,11 +16,16 @@ namespace dollars2zingers
     {
         static void Main()
 		{
-			//Amount of KFC Zingers buyable with user defined money input
-			const decimal ZING_COST = 5.95m;
-			decimal outputQ;		//output quotient variable
-			decimal outputR;			//output remainder variable
-			//decimal place checking, lifted from microsoft http://bit.ly/2uQuXgS
+            //initialize variables
+			const decimal ZING_COST = 5.95m;        //Amount of KFC Zingers buyable with user defined money input
+            decimal outputQ;		//output quotient variable
+           
+			decimal outputR;            //output remainder variable
+            string qString;      //string of outputQ
+            string rString;     //string of outputR
+            string numString;        //string of number
+            
+            //decimal place checking, lifted from microsoft http://bit.ly/2uQuXgS
 			decimal number = 0m;		//input number, with decimal place checking
 			bool valid = false;
 			while(!valid)
@@ -38,18 +43,25 @@ namespace dollars2zingers
 					
 				}
 			}
-			
-			//Actual interesting stuff begins here
-			
-			//Take budget, divides by ZING_COST for total number, and
-			//calculates the remainder through modulus
-			outputQ = number / ZING_COST;
-			outputR = number % ZING_COST;
-			 
-            //need to round the number of zingers too
 
-			//Writes output to prompt
-			Console.WriteLine("The amount of Zingers you can buy for $" + number.ToString("C") + " is " + outputQ.ToString("C") + " with " + outputR.ToString("C") + " leftover");
-		}
+            //Actual interesting stuff begins here
+
+            //Take budget, divides by ZING_COST for total number, and
+            //calculates the remainder through modulus
+            outputQ = number / ZING_COST;
+			outputR = number % ZING_COST;
+
+            //round down and convert outputQ to intQ
+            outputQ = Decimal.Floor(outputQ);
+            outputQ = Decimal.ToInt32(outputQ);
+        //TODO figure out how to drop the tailing zeroes when outputQ is converted to a string
+
+            //converts variables from decimal values to strings
+            qString = outputQ.ToString("C");
+            rString = outputR.ToString("C");
+            numString = number.ToString("C");
+            //Writes output to prompt
+            Console.WriteLine("The amount of Zingers you can buy for ${0} is {1} with ${2} leftover", numString, qString, rString );
+        }
 	}
 }
