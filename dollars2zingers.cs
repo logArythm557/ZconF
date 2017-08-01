56445939214793ead91616f9bd75b8c00e19a50a
logArythm557 <jake.ewhite@gmail.com>
2017-08-01 17:31:14 +0930

    Add files via upload
    needs to be fixed

---
+++ dollars2zingers.cs
@@ -0,0 +1,53 @@
+//Dollars2Zingers.cs
+
+//A program that tells you how many Zingers
+//you can buy in adelaide with the amount of money you have.
+//it's just as dumb as it sounds
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dollars2zingers
+{
+    class d2z
+    {
+        static void Main()
+		{
+			//Amount of KFC Zingers buyable with user defined money input
+			const decimal ZING_COST = 5.95m;
+			decimal outputQ;		//output quotient variable
+			decimal outputR;			//output remainder variable
+			//decimal place checking, lifted from microsoft http://bit.ly/2uQuXgS
+			decimal number = 0m;		//input number, with decimal place checking
+			bool valid = false;
+			while(!valid)
+			{
+				Console.WriteLine("Please enter your Zinger budget: $");
+				string input = Console.ReadLine();
+				if(decimal.TryParse(input, out number))
+				{
+					number = Math.Round(number, 2);		//rounds the number variable to 2 places
+					valid = true;
+				}
+				else
+				{
+					Console.WriteLine("Please list your Zinger budget in numbers");
+					
+				}
+			}
+			
+			//Actual interesting stuff begins here
+			
+			//Take budget, divides by ZING_COST for total number, and
+			//calculates the remainder through modulus
+			outputQ = number / ZING_COST;
+			outputR = number % ZING_COST;
+			
+			//Writes output to prompt
+			Console.WriteLine("The amount of Zingers you can buy for $" + number.ToString("C") " is" + outputQ.ToString("C") "with " + outputR.ToString("C") "leftover");
+		}
+	}
+}
